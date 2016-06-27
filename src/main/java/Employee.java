d3cf77eb1c3f4eac84192637076963ab18f26cb5
Vitor Pereira <rotiv695@gmail.com>
2016-06-27 17:36:47 +0100

    clonning objects

---
+++ src/main/java/Employee.java
@@ -0,0 +1,58 @@
+public class Employee implements Cloneable{
+
+		private String lastName;
+		private String firstName;
+		private double salary;
+		
+		public Address address;
+		
+		public Employee(String lastName, String firstName){
+			
+			this.lastName = lastName;
+			this.firstName = firstName;
+			
+		}
+		
+		public String getLastName(){
+			return this.lastName;
+		}
+		
+		public void setLastName(String lastName){
+			this.lastName = lastName;
+		}
+		
+		public String getFirstName(){
+			return this.firstName;
+		}
+		
+		public void setFirstName(String firstName){
+			this.firstName = firstName;
+		}
+		
+		public double getSalary(){
+			return this.salary;
+		}
+		
+		public void setSalary(double salary){
+			this.salary = salary;
+		}
+		
+		public Object clone(){
+			
+			Employee emp;
+			try{
+				emp = (Employee) super.clone();
+				emp.address = (Address) address.clone();
+			}
+			catch(CloneNotSupportedException e){
+				return null;
+			}
+			
+			return emp;
+		}
+		
+		public String toString(){
+			
+			return this.getClass().getName() + "[" + this.firstName + " " + this.lastName + " "+ this.salary+" ]";
+		}
+}
---
+++ src/test/java/CloneTest.java
@@ -0,0 +1,31 @@
+public class CloneTest {
+
+	public static void main(String[] args){
+		
+		Employee emp1 = new Employee ("Martinez", "Anthony");
+		emp1.setSalary(40000.0);
+		
+		emp1.address = new Address("1300 first street", "Fresno", "CA", "93702");
+		
+		Employee emp2 = (Employee) emp1.clone();
+		
+		System.out.println("***** after cloning *****\n");
+		printEmployee(emp1);
+		System.out.println();
+		printEmployee(emp2);
+		
+		emp2.setLastName("Smith");
+		emp2.address = new Address("205 6th street", "Fresno", "CA", "93722");
+		
+		System.out.println("***** after changing emp2 *****\n");
+		printEmployee(emp1);
+		System.out.println();
+		printEmployee(emp2);
+	}
+	
+	public static void printEmployee (Employee e){
+		
+		System.out.println(e.getFirstName()+ " "+e.getLastName());
+		System.out.println(e.address.getAddress());
+	}
+}
